b61fef5028e31df4259c0f5b6986b1106c949e52
Wiktoria <wiktoriabanach@op.pl>
2025-05-16 15:53:36 +0200

    Apply dynamic styling

--- 01 React Essentials/src/components/TabButton.jsx
+++ 01 React Essentials/src/components/TabButton.jsx
@@ -1,6 +1,7 @@
-export default function TabButton({children, onSelect}) {
-
-    return (
-        <li><button onClick={onSelect}>{children}</button></li>
-    )
+export default function TabButton({ children, onSelect, isSelected }) {
+  return (
+    <li>
+      <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
+    </li>
+  );
 }
