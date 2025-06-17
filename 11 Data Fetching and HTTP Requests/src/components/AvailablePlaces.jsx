c37522a50c2fbdf480c3465e7bc89da4e9ea17bf
Wiktoria Banach <115671088+Wikuska@users.noreply.github.com>
2025-06-17 17:36:57 +0200

    GET data from server by sending HTTP request

--- 11 Data Fetching and HTTP Requests/src/components/AvailablePlaces.jsx
+++ 11 Data Fetching and HTTP Requests/src/components/AvailablePlaces.jsx
@@ -1,8 +1,18 @@
-import { useState } from 'react';
-import Places from './Places.jsx';
+import { useState, useEffect } from "react";
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
-const [avaliablePlaces, setAvaliablePlaces] = useState([])
+  const [avaliablePlaces, setAvaliablePlaces] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/places")
+      .then((response) => {
+        return response.json();
+      })
+      .then((resData) => {
+        setAvaliablePlaces(resData.places);
+      });
+  }, []);
 
   return (
     <Places
