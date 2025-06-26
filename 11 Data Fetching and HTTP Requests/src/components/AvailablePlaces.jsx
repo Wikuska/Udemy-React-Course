9fe750746e2794c4aede6673ac0f3476f88dc908
Wiktoria <wiktoriabanach@op.pl>
2025-06-26 14:35:41 +0200

    Send data with POST request

--- 11 Data Fetching and HTTP Requests/src/components/AvailablePlaces.jsx
+++ 11 Data Fetching and HTTP Requests/src/components/AvailablePlaces.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 import ErrorPage from "./Error.jsx";
 import { sortPlacesByDistance } from "../loc.js";
-import { fetchAvaliablePlaces } from "../http.js";
+import { fetchAvailablePlaces } from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -14,7 +14,7 @@ export default function AvailablePlaces({ onSelectPlace }) {
       setIsFetching(true);
 
       try {
-        const places = await fetchAvaliablePlaces();
+        const places = await fetchAvailablePlaces();
 
         navigator.geolocation.getCurrentPosition((position) => {
           const sortedPlaces = sortPlacesByDistance(
