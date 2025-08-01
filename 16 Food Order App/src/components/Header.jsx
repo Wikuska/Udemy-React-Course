5b7e85c0216354f3836708b4d2420dabe0a89333
Wiktoria <wiktoriabanach@op.pl>
2025-08-01 15:23:33 +0200

    Use CartContext in Header and MealItem

--- 16 Food Order App/src/components/Header.jsx
+++ 16 Food Order App/src/components/Header.jsx
@@ -1,11 +1,20 @@
+import { useContext } from "react";
+import CartContext from "../store/CartContext";
+
 export default function Header() {
+  const cartCtx = useContext(CartContext);
+
+  const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+    return totalNumberOfItems + item.quantity;
+  }, 0);
+
   return (
     <div id="main-header">
       <div id="title">
         <img src="logo.jpg" alt="logo" />
         <h1>REACTFOOD</h1>
       </div>
-      <button className="text-button">Cart</button>
+      <button className="text-button">Cart({totalCartItems})</button>
     </div>
   );
 }
--- 16 Food Order App/src/components/MealItem.jsx
+++ 16 Food Order App/src/components/MealItem.jsx
@@ -1,17 +1,27 @@
+import { useContext } from "react";
 import { currencyFormatter } from "../util/formatting";
+import CartContext from "../store/CartContext";
 
-export default function MealItem({
-  meal: { name, price, description, image },
-}) {
+export default function MealItem({ meal }) {
+  const cartCtx = useContext(CartContext);
+
+  function handleAddMealToCart() {
+    cartCtx.addItem(meal);
+  }
+
   return (
     <div className="meal-item">
       <article>
-        <img src={`http://localhost:3000/${image}`} alt="meal picture" />
-        <h3>{name}</h3>
+        <img src={`http://localhost:3000/${meal.image}`} alt="meal picture" />
+        <h3>{meal.name}</h3>
         <div className="meal-item-actions">
-          <p className="meal-item-price">{currencyFormatter.format(price)}</p>
-          <p className="meal-item-description">{description}</p>
-          <button className="button">Add to Cart</button>
+          <p className="meal-item-price">
+            {currencyFormatter.format(meal.price)}
+          </p>
+          <p className="meal-item-description">{meal.description}</p>
+          <button className="button" onClick={handleAddMealToCart}>
+            Add to Cart
+          </button>
         </div>
       </article>
     </div>
