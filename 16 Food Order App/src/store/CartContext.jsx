302937f7999488ed8c347fc09f16b855bdc2af7f
Wiktoria <wiktoriabanach@op.pl>
2025-08-01 15:04:40 +0200

    In CartContext add possibility to remove item

--- 16 Food Order App/src/store/CartContext.jsx
+++ 16 Food Order App/src/store/CartContext.jsx
@@ -31,15 +31,43 @@ function cartReducer(state, action) {
   }
 
   if (action.type == "REMOVE_ITEM") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    const updatedItems = [...state.items];
+
+    if (existingCartItem.quantity === 1) {
+      updatedItems.splice(existingCartItemIndex, 1);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity - 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+    return { ...state, items: updatedItems };
   }
 
   return state;
 }
 
 export function CartContextProvider({ children }) {
-  useReducer(cartReducer, { items: [] });
+  const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
 
-  return <CartContext>{children}</CartContext>;
+  function addItem(item) {
+    dispatchCartAction({ type: "ADD_ITEM", item });
+  }
+
+  function removeItem(id) {
+    dispatchCartAction({ type: "REMOVE_ITEM", id });
+  }
+
+  const cartContext = { items: cart.items, addItem, removeItem };
+
+  return <CartContext value={cartContext}>{children}</CartContext>;
 }
 
 export default CartContext;
