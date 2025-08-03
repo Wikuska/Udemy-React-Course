fb8f480fc51a2c07c34b14f1cafdaeb1a3f05d00
Wiktoria <wiktoriabanach@op.pl>
2025-08-03 17:44:47 +0200

    Handle form submission and send POST request

--- 16 Food Order App/src/components/Checkout.jsx
+++ 16 Food Order App/src/components/Checkout.jsx
@@ -13,9 +13,33 @@ export default function Checkout() {
     return totalPrice + item.quantity * item.price;
   }, 0);
 
+  function handleClose() {
+    userProgressCtx.hideCheckout();
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries()); // { email: test@example.com }
+
+    fetch("http://localhost:3000/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        order: {
+          items: cartCtx.items,
+          customer: customerData,
+        },
+      }),
+    });
+  }
+
   return (
-    <Modal open={userProgressCtx.progress === "checkout"}>
-      <form>
+    <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
 
@@ -28,7 +52,9 @@ export default function Checkout() {
         </div>
 
         <p className="modal-actions">
-          <button className="text-button">Close</button>
+          <button className="text-button" onClose={handleClose}>
+            Close
+          </button>
           <button className="button">Submit Order</button>
         </p>
       </form>
