f2ff9217e489aa1b32106ad5df901f8ab4cb3363
Wiktoria <wiktoriabanach@op.pl>
2025-08-03 18:03:04 +0200

    Create custom HTTP hook

--- 16 Food Order App/src/components/Meals.jsx
+++ 16 Food Order App/src/components/Meals.jsx
@@ -1,21 +1,26 @@
-import { useEffect, useState } from "react";
-import MealItem from "./MealItem";
+import MealItem from "./MealItem.jsx";
+import useHttp from "../hooks/useHttp.js";
+
+const requestConfig = {};
 
 export default function Meals() {
-  const [meals, setMeals] = useState([]);
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp("http://localhost:3000/meals", requestConfig, []);
 
-  useEffect(() => {
-    async function fetchMeals() {
-      const response = await fetch("http://localhost:3000/meals");
-      const meals = await response.json();
-      setMeals(meals);
-    }
-    fetchMeals();
-  }, []);
+  if (isLoading) {
+    return <p>Fetching meals...</p>;
+  }
+
+  // if (!data) {
+  //   return <p>No meals found.</p>
+  // }
 
   return (
     <ul id="meals">
-      {meals.map((meal) => (
+      {loadedMeals.map((meal) => (
         <MealItem key={meal.id} meal={meal} />
       ))}
     </ul>
